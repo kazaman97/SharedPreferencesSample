157f3c24526506da58d019838b9ad59f8bcee04f
kazaman97 <narusai97@gmail.com>
2021-10-14 22:13:18 +0900

    add impl of delete

--- app/src/main/java/com/github/kazaman97/shared_preferences_sample/MainActivity.kt
+++ app/src/main/java/com/github/kazaman97/shared_preferences_sample/MainActivity.kt
@@ -6,15 +6,26 @@ import androidx.databinding.DataBindingUtil
 import com.github.kazaman97.shared_preferences_sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val SP_KEY = "memo"
+    }
+
     private val binding: ActivityMainBinding by lazy {
         DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 
+    private val sharedPreferencesManager: SharedPreferencesManager
+        get() = SharedPreferencesManager.instance
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding.textField.setText(SharedPreferencesManager.instance.getString("memo"))
-        binding.button.setOnClickListener {
-            SharedPreferencesManager.instance.putString("memo", binding.textField.text.toString())
+        binding.textField.setText(sharedPreferencesManager.getString(SP_KEY))
+        binding.save.setOnClickListener {
+            sharedPreferencesManager.putString(SP_KEY, binding.textField.text.toString())
+        }
+        binding.delete.setOnClickListener {
+            sharedPreferencesManager.removeString(SP_KEY)
+            binding.textField.setText("")
         }
     }
 }
